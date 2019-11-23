87a945870510d6914307821aa039d6fde606e640
josephinetherese <josdougl@indiana.edu>
2019-11-23 04:18:32 -0500

    cleans up existing code, adds more generic handling of Cas, poss.subj

--- Word.py
+++ Word.py
@@ -68,6 +68,7 @@ class Word:
         if self.deprel() in suffixes:
             self._xpos = 'SUFFIX'
         self.convert_misc()
+        self.empty_feats()
 
     def convert_form(self):
         self._form = self._form.replace('\"', '@quot')
@@ -79,6 +80,9 @@ class Word:
             self._misc.append('FEATURES=[' + self._feats[0] + ']')
         else:
             self._misc.append('FEATURES')
+
+    def empty_feats(self):
+        self._feats = []
 
     def cleanup(self):
         self.cleanup_form()
@@ -102,7 +106,8 @@ class Word:
                     misc_items[left_side] = item.replace(left_side + '=', '')
         for item in misc_items:
             new_misc.append(item + '=[' + misc_items[item] + ']')
-        self._misc = new_misc
+        # self._misc = new_misc
+        self._misc = ''
 
     def get_FEATURES(self, item):
         item = item.replace('true', '').replace(
@@ -112,18 +117,24 @@ class Word:
             features = item.split('&')
             for feature in features:
                 feature = feature.replace('[', '').replace(']', '')
+                feature = feature.replace('psor', '[psor]')
+                feature = feature.replace('subj', '[subj]')
                 cleaned.append(feature)
         return cleaned
 
     def convert_deprel(self):
-        if self._deprel in deprel_dict:
-            self._deprel = deprel_dict[self._deprel]
-        if 'Gloss=ser' in self._misc:
-            self._deprel = 'ser'
+        if 'Gloss=ser' in self._misc or self._form.lower() == "kan":
+            self._deprel = 'cop'
+            self._upos = 'AUX'
         if self._deprel == 'neg':
             self._upos = 'NEG'
-        if self._deprel == '_':
-            self._deprel = 'none'
+        elif self._deprel == '--':
+            if self._upos == 'SP':
+                self._deprel = 'flat:foreign'
+            else:
+                self._deprel = 'flat'
+        if self._deprel in deprel_dict:
+            self._deprel = deprel_dict[self._deprel]
 
     def convert_xpos(self):
         if self._xpos in xpos_dict:
@@ -132,46 +143,13 @@ class Word:
     def convert_feats(self):
         self._misc.append('Feats=' + '|'.join(self._feats))
         new_features = []
-        extra_feats = []
-        for feat in self._feats:
-            feat = feat.strip()
-            if '.' in feat and feat not in feats_dict and not feat == 'PrnPers+3.Sg' and not feat == 'PrnPers+2.Sg':
-                split_feat = feat.split('.')
-                if len(split_feat) > 1:
-                    extra_feats.extend(split_feat)
-        if 'Incl' in extra_feats:
-            if '+1' in extra_feats:
-                extra_feats.append('Person=1+INCL')
-                extra_feats.remove('+1')
-                extra_feats.remove('Incl')
-        elif 'Excl' in extra_feats:
-            if '+1' in extra_feats:
-                extra_feats.append('Person=1+EXCL')
-                extra_feats.remove('+1')
-                extra_feats.remove('Excl')
-        self._feats.extend(extra_feats)
         for feat in self._feats:
             if self._upos not in ud_pos_tags:
-                if feat == 'VRoot' or feat == 'Root_VS' or feat == 'VRootES':
-                    self._upos = 'VERB'
-                elif feat == 'NRootES' or feat == 'NRoot':
-                    self._upos = 'NOUN'
-                elif feat == 'NRootNUM':
-                    self._upos = 'NUM'
-                elif feat == 'PrnDem':
-                    self._upos = 'DET'
-                elif feat == '+SS' or feat == 'SS':
-                    self._deprel = 'advcl:ss'
-                elif feat == '+DS' or feat == 'DS':
-                    self._deprel = 'advcl:ds'
-                elif feat == 'Part_Neg':
-                    self._upos = 'NEG'
-            if feat == 'PrnPers+3.Sg' and self._form.lower() == 'paykuna':
-                new_features.append('Person=3')
-                new_features.append('PronType=Prs')
-            elif feat == 'PrnPers+2.Sg' and self._form.lower() == 'qamkuna':
-                new_features.append('Person=2')
-                new_features.append('PronType=Prs')
+                self.feat_to_upos(feat)
+            self.feat_to_deprel(feat)
+            more_feats = self.check_specials(feat)
+            if more_feats:
+                new_features.extend(more_feats)
             else:
                 if feat in feats_dict:
                     from_dict = feats_dict[feat]
@@ -179,9 +157,43 @@ class Word:
                         new_features.extend(from_dict.split('|'))
                     else:
                         new_features.append(from_dict)
-        self._feats = copy.deepcopy(new_features)
+        self._feats = copy.deepcopy(list(set(new_features)))
+
+    def feat_to_upos(self, feat):
+        if feat == 'VRoot' or feat == 'Root_VS' or feat == 'VRootES':
+            self._upos = 'VERB'
+        elif feat == 'NRootES' or feat == 'NRoot':
+            self._upos = 'NOUN'
+        elif feat == 'NRootNUM':
+            self._upos = 'NUM'
+        elif feat == 'PrnDem':
+            self._upos = 'DET'
+        elif feat == 'Part_Neg':
+            self._upos = 'NEG'
+        elif feat == 'Part_Contr':
+            self._upos = 'PART'
+        elif feat == 'NP':
+            self._upos = 'NOUN'
+
+    def feat_to_deprel(self, feat):
+        if feat == '+SS' or feat == 'SS':
+            self._deprel = 'advcl:ss'
+        elif feat == '+DS' or feat == 'DS':
+            self._deprel = 'advcl:ds'
+
+    def check_specials(self, feat):
+        out = None
+        if feat == 'PrnPers+3.Sg' and self._form.lower() == 'paykuna':
+            out = ['Person=3', 'PronType=Prs']
+        elif feat == 'PrnPers+2.Sg' and self._form.lower() == 'qamkuna':
+            out = ['Person=3', 'PronType=Prs']
+        return out
 
     def convert_upos(self):
+        if self._upos == 'FLM':
+            self._feats.append('Foreign=Yes')
+        elif self._form.lower() == 'hina':
+            self._upos = 'ADP'
         if self._upos in upos_dict:
             self._upos = upos_dict[self._upos]
 
@@ -216,7 +228,7 @@ class Word:
             feat = feat.replace('|', '')
             if len(feat) > 0:
                 new_feats.append(feat)
-        self._feats = new_feats
+        self._feats = list(set(new_feats))
         if self._feats:
             self._feats.sort()
 
@@ -257,7 +269,13 @@ deprel_dict = {
     # 'sntc': 'root',
     'det': 'det',
     'goal': 'goal',
-    'hab': 'hab'
+    'hab': 'hab',
+    'qnt': 'nummod',
+    'nme': 'flat',
+    'poss': 'case',
+    'flm': 'flat:foreign',
+    'p.arg': 'case',
+    '--': 'flat'
 
 }
 
@@ -274,7 +292,12 @@ upos_dict = {
     'NRootNUM': 'NUM',
     'Root_VS': 'VERB',
     'VRootES': 'VERB',
-    'NRootES': 'NOUN'
+    'NRootES': 'NOUN',
+    'FLM': 'X',
+    'DUMMY': 'X',
+    '$.': 'PUNCT',
+    'SP': 'X',
+    'NDeriv': 'NOUN'
 }
 
 feats_dict = {
@@ -301,6 +324,7 @@ feats_dict = {
     'FLM': 'Foreign=Yes',
     '+Foc': 'Focus=Yes',
     '+Fut': 'Tense=Fut',
+    'Foreign=Yes': 'Foreign=Yes',
     '+Gen': 'Case=Gen',
     '+Hab': 'Tense=Past|Aspect=Hab',
     '+Imp': 'Mood=Imp',
@@ -310,6 +334,7 @@ feats_dict = {
     '+IPst': 'Tense=Past|Evident=IndE',
     '+Instr': 'Case=Ins',
     '+Lim': 'Case=Lim',
+    '+Lim_Aff': 'Case=Lim|Mood=Affective',
     '+Loc': 'Case=Loc',
     '_Neg': 'Polarity=Neg',
     '+Obl': 'Mood=Obligative',
@@ -347,6 +372,10 @@ feats_dict = {
     '+1.Pl.Poss': 'Number[psor]=Plur|Person[psor]=1',
     '+1.Pl.Excl.Poss': 'Person[psor]=1|Inclusive[psor]=No|Number[psor]=Plur',
     '+1.Pl.Incl.Poss': 'Person[psor]=1|Inclusive[psor]=Yes|Number[psor]=Plur',
+    '+1.Pl.Excl.Subj': 'Person[subj]=1|Inclusive[subj]=No|Number[subj]=Plur',
+    '+1.Pl.Incl.Subj': 'Person[subj]=1|Inclusive[subj]=Yes|Number[subj]=Plur',
+    '+1.Pl.Incl.Subj.Fut': 'Person[subj]=1|Inclusive[subj]=Yes|Number[subj]=Plur|Tense=Fut',
+    '+1.Pl.Excl.Subj.Fut': 'Person[subj]=1|Inclusive[subj]=No|Number[subj]=Plur|Tense=Fut',
     '+Ass': 'Mood=Assistive',
     '+Autotrs': '',  # verbalizing suffix, can probably leave blank
     '+Emph': 'PronType=Emp',
